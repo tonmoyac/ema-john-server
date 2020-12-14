c7e9df3a23916daa43e321026ca876fdd3bd27c6
tonmoyac <67514778+tonmoyac@users.noreply.github.com>
2020-12-14 22:43:39 +0600

    Ema-John Server

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv').config()
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zqzli.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express()
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const productsCollection = client.db("EmaJohn").collection("products");
+  const ordersCollection = client.db("EmaJohn").collection("orders");
+
+  app.post('/addProduct', (req, res) => {
+      const products = req.body;
+      productsCollection.insertOne(products)
+      .then(result => {
+          res.send(result.insertedCount)
+          console.log(result.insertedCount)
+      })
+  })
+  app.get('/products', (req, res) => {
+      productsCollection.find({})
+      .toArray((err, documents) => {
+          res.send(documents);
+      })
+  })
+  app.get('/product/:key', (req, res) => {
+      productsCollection.find({key: req.params.key})
+      .toArray((err, documents) => {
+          res.send(documents[0]);
+      })
+  })
+  app.post('/productsByKeys', (req, res) => {
+      const productsKeys = req.body;
+      productsCollection.find({key: { $in: productsKeys} })
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  })
+  app.post('/addOrder', (req, res) => {
+    const order = req.body;
+    ordersCollection.insertOne(order)
+    .then(result => {
+        res.send(result.insertedCount > 0)
+    })
+})
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
